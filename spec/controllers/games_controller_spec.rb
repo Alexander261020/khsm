07056545fff9af95f69f2b67d361f5c27aaeafac
alexandr <89227049120@mail.ru>
2021-12-29 14:34:39 +0500

    spec take_money

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -29,6 +29,37 @@ RSpec.describe GamesController, type: :controller do
     # Логиним юзера с помощью девайзовского метода sign_in
     before(:each) { sign_in user }
   
+    # юзер берет деньги
+    it 'takes money' do
+      # вручную поднимем уровень вопроса до выигрыша 200
+      game_w_questions.update_attribute(:current_level, 2)
+
+      post :take_money, id: game_w_questions.id
+      game = assigns(:game)
+      expect(game.finished?).to be_truthy
+      expect(game.prize).to eq(200)
+
+      # пользователь изменился в базе, надо в коде перезагрузить!
+      user.reload
+      expect(user.balance).to eq(200)
+
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:warning]).to be
+    end
+
+    # проверка, что пользовтеля посылают из чужой игры
+    it '#show alien game' do
+      # создаем новую игру, юзер не прописан, будет создан фабрикой новый
+      alien_game = FactoryGirl.create(:game_with_questions)
+
+      # пробуем зайти на эту игру текущий залогиненным user
+      get :show, id: alien_game.id
+
+      expect(response.status).not_to eq(200) # статус не 200 ОК
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to be # во flash должен быть прописана ошибка
+    end
+
     it 'creates game' do
       # Создадим пачку вопросов
       generate_questions(15)
