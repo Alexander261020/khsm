2098677a6a516bd56c7b5450a2012acdcc8d83cd
alexandr <89227049120@mail.ru>
2022-01-06 15:36:56 +0500

    add check method add_friend_call

--- app/models/game_question.rb
+++ app/models/game_question.rb
@@ -1,39 +1,28 @@
-#  (c) goodprogrammer.ru
-#
+# (c) goodprogrammer.ru
 require 'game_help_generator'
+
 # Игровой вопрос — модель, которая связывает игру и вопрос. При создании новой
 # игры формируется массив из 15 игровых вопросов для конкретной игры.
 class GameQuestion < ActiveRecord::Base
-  # Игровой вопрос, конечно, принадлежит конкретной игре.
   belongs_to :game
 
-  # Игровой вопрос знает, из какого вопроса берется информация
   belongs_to :question
 
-  # Создаем в этой модели виртуальные геттеры text, level, значения которых
-  # автоматически берутся из связанной модели question.
-  #
-  # Таким обазом при вызове, например
-  #
-  # game_question.text
-  #
-  # получим то, что лежит в
-  #
-  # game_question.question.text
-  #
   delegate :text, :level, to: :question, allow_nil: true
 
-  # Без игры и вопроса — игровой вопрос не имеет смысла
   validates :game, :question, presence: true
 
-  # В полях a, b, c и d прячутся индексы ответов из объекта :game. Каждый из
-  # них — целое число от 1 до 4.
   validates :a, :b, :c, :d, inclusion: {in: 1..4}
 
+  # Автоматическая сериализация поля в базу (мы юзаем как обычный хэш, а
+  # рельсы в базе хранят как строчку).
+  #
+  # См. ссылки в материалах урока
   serialize :help_hash, Hash
-  
+
+  # help_hash у нас имеет такой формат:
   # {
-  #   # При использовании подсказки остались варианты a и b
+  #   # При использовании подсказски остались варианты a и b
   #   fifty_fifty: ['a', 'b'],
   #
   #   # Распределение голосов по вариантам a, b, c, d
@@ -42,37 +31,6 @@ class GameQuestion < ActiveRecord::Base
   #   # Друг решил, что правильный ответ А (просто пишем текстом)
   #   friend_call: 'Василий Петрович считает, что правильный ответ A'
   # }
-
-
-  # Основные методы для доступа к данным в шаблонах и контроллерах:
-
-  # Метод variants возвращает хэш с ключами a..d и значениями — тектом ответов:
-  #
-  # {
-  #   'a' => 'Текст ответа Х',
-  #   'b' => 'Текст ответа У',
-  #   ...
-  # }
-
-  # Метод answer_correct? проверяет правильность ответа по букве. Возвращает
-  # true, если переданная буква (строка или символ) содержит верный ответ и
-  # false во всех других случаях.
-  def answer_correct?(letter)
-    correct_answer_key == letter.to_s.downcase
-  end
-
-  # Метод correct_answer_key возвращает ключ правильного ответа 'a', 'b', 'c',
-  # или 'd'. Обратите внимание, что в переменных a, b, c и d игрового вопроса
-  # лежат числа от 1 до 4, но мы не знаем, в какой букве какое число.
-  def correct_answer_key
-    {a => 'a', b => 'b', c => 'c', d => 'd'}[1]
-  end
-
-  # Метод correct_answer возвращает текст правильного ответа
-  def correct_answer
-    variants[correct_answer_key]
-  end
-
   def variants
     {
       'a' => question.read_attribute("answer#{a}"),
@@ -80,6 +38,29 @@ class GameQuestion < ActiveRecord::Base
       'c' => question.read_attribute("answer#{c}"),
       'd' => question.read_attribute("answer#{d}")
     }
+  end
+
+  def answer_correct?(letter)
+    correct_answer_key == letter.to_s.downcase
+  end
+
+  def correct_answer_key
+    {a => 'a', b => 'b', c => 'c', d => 'd'}[1]
+  end
+
+  def correct_answer
+    variants[correct_answer_key]
+  end
+
+  # Добавляем в help_hash по ключю fifty_fifty — массив из двух вариантов:
+  # правильный и случайный и сохраняем объект.
+  def add_fifty_fifty
+    self.help_hash[:fifty_fifty] = [
+      correct_answer_key,
+      (%w(a b c d) - [correct_answer_key]).sample
+    ]
+
+    save
   end
 
   # Генерируем в help_hash случайное распределение по вариантам и сохраняем объект
@@ -93,8 +74,20 @@ class GameQuestion < ActiveRecord::Base
     save
   end
 
+  # Добавляем в help_hash подсказку друга и сохраняем объект
+  def add_friend_call
+    # Массив ключей
+    keys_to_use = keys_to_use_in_help
+
+    self.help_hash[:friend_call] =
+      GameHelpGenerator.friend_call(keys_to_use, correct_answer_key)
+
+    save
+  end
+
   private
 
+  # Рассчитываем какие ключи нам доступны в подсказках
   def keys_to_use_in_help
     keys_to_use = variants.keys
 
