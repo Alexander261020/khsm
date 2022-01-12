488d02167134c01521cfeb151d0a5b4eae0a1536
alexandr <89227049120@mail.ru>
2022-01-12 10:13:44 +0500

    user page template test

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe 'users/show', type: :view do
     current_user = assign(:user, FactoryGirl.build_stubbed(:user, name: 'NameUser', balance: 5000))
     # Разрешаем объекту view в ответ на вызов current_user возвращать current_user
     allow(view).to receive(:current_user).and_return(current_user)
+    # создаем игру
+    assign(:games, [FactoryGirl.build_stubbed(:game)])
+    stub_template 'users/_game.html.erb' => 'User game goes here'
 
     render
   end
@@ -20,5 +23,10 @@ RSpec.describe 'users/show', type: :view do
   # Этот сценарий проверяет, что шаблон выводит смену имени и пароля
   it 'checking the rendering of the password change button' do
     expect(rendered).to match 'Сменить имя и пароль'
+  end
+
+  # Проверяем отрисовку фрагмента с игрой
+  it 'check a fragment with the game is drawn' do
+    expect(rendered).to match 'User game goes here'
   end
 end
